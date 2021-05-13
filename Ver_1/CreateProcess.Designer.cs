b6b8eaf2fc0994a4e7dd7047f76e47e5db476355
Apple1501 <kunure15@gmail.com>
2021-05-13 16:39:15 +0300

    Загрузка данных из первой таблицы

--- Ver_1/CreateProcess.Designer.cs
+++ Ver_1/CreateProcess.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.panelCreateProcess = new System.Windows.Forms.Panel();
+            this.LoadInfoOper = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -44,7 +45,6 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SaveNewProcess = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.button1 = new System.Windows.Forms.Button();
             this.panelCreateProcess.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -52,7 +52,7 @@
             // panelCreateProcess
             // 
             this.panelCreateProcess.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panelCreateProcess.Controls.Add(this.button1);
+            this.panelCreateProcess.Controls.Add(this.LoadInfoOper);
             this.panelCreateProcess.Controls.Add(this.label3);
             this.panelCreateProcess.Controls.Add(this.button5);
             this.panelCreateProcess.Controls.Add(this.button4);
@@ -64,10 +64,21 @@
             this.panelCreateProcess.Controls.Add(this.textBox1);
             this.panelCreateProcess.Controls.Add(this.SaveNewProcess);
             this.panelCreateProcess.Controls.Add(this.listView1);
-            this.panelCreateProcess.Location = new System.Drawing.Point(0, 1);
+            this.panelCreateProcess.Location = new System.Drawing.Point(0, -2);
             this.panelCreateProcess.Name = "panelCreateProcess";
-            this.panelCreateProcess.Size = new System.Drawing.Size(1242, 717);
+            this.panelCreateProcess.Size = new System.Drawing.Size(1239, 763);
             this.panelCreateProcess.TabIndex = 0;
+            // 
+            // LoadInfoOper
+            // 
+            this.LoadInfoOper.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoadInfoOper.Location = new System.Drawing.Point(3, 35);
+            this.LoadInfoOper.Name = "LoadInfoOper";
+            this.LoadInfoOper.Size = new System.Drawing.Size(371, 51);
+            this.LoadInfoOper.TabIndex = 12;
+            this.LoadInfoOper.Text = "Загрузить данные об операциях";
+            this.LoadInfoOper.UseVisualStyleBackColor = true;
+            this.LoadInfoOper.Click += new System.EventHandler(this.LoadInfoOper_Click);
             // 
             // label3
             // 
@@ -82,7 +93,7 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button5.Location = new System.Drawing.Point(882, 663);
+            this.button5.Location = new System.Drawing.Point(849, 663);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(360, 51);
             this.button5.TabIndex = 10;
@@ -198,16 +209,6 @@
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(12, 11);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(360, 51);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "Добавить операцию";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // CreateProcess
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -241,6 +242,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn IDKODOPE;
         private System.Windows.Forms.DataGridViewTextBoxColumn NameOp;
         private System.Windows.Forms.DataGridViewTextBoxColumn Time;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button LoadInfoOper;
     }
 }
--- Ver_1/CreateProcess.cs
+++ Ver_1/CreateProcess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,6 +25,38 @@ namespace Ver_1
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+        }
+        
+        //Загружаем данные об операция из базы данных
+        private void LoadInfoOper_Click(object sender, EventArgs e)
+        {
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            //формируем запрос к базе данных
+            MySqlCommand command = new MySqlCommand("SELECT OperName,WorkerName,ToolName FROM `mpmoperation`",db.getConnection());
+
+            //указываем какой именно запрос будет выполняться к базе данных
+            adapter.SelectCommand = command;
+
+            //заполняем таблицу данными, которые были загружены из бд
+            adapter.Fill(table);
+
+            //проверка, что в базе данных есть информация
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("Данные из базы данных загружены");
+            }
+            else
+            {
+                MessageBox.Show("В базе данных нет информации");
+            }
+            int a = table.Rows.Count;
+
+
 
         }
     }
